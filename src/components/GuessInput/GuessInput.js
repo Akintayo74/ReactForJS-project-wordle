4dd66f310a716c25fd392833ef78f8f123223dd9
Akintayo74 <akinnibosunakintayo74@gmail.com>
2025-06-04 14:08:14 +0100

    Created GuessInput component. Created form with text input and implemented two way data binding using React State

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
 function GuessInput() {
-  return <div></div>;
+  const [guessInput, setGuessInput] = React.useState('');
+  return (
+    <>
+      <form 
+        className="guess-input-wrapper"
+        onSubmit={(event) => {
+          event.preventDefault();
+          console.log(guessInput);
+        }}
+      > 
+        <label for="guess-input">Enter guess:</label>
+        <input 
+          id="guess-input" 
+          type="text"
+          value={guessInput}
+          onChange={(event) => {
+            setGuessInput(event.target.value)
+          }}
+        />
+      </form>
+    </>
+  );
 }
 
 export default GuessInput;
